6cc74e7e682880db45c96b2a23a1b44e8da4f80a
Nandhitha-S19 <nandhitha.s1922@gmail.com>
2026-02-15 21:36:35 +0530

    15-02-2025

---
+++ Round2/src/Pattern1.java
@@ -0,0 +1,31 @@
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        int n = 3;
+        int num = 0;
+        int i = 1;
+        for(; i <= n;i++){
+                int l = num+i;
+                num =l;
+                int j = 0;
+                while((l >= i) && (j < i)){
+                    System.out.print(l+ " ");
+                    l--;
+                    j++;
+                }
+            System.out.println();
+        }
+        int j = n-1;
+        for(; j >= 1;j--){
+            int l = num+j;
+            num =l;
+            int k = 0;
+            while((l >= j) && (k < j)){
+                System.out.print(l+ " ");
+                l--;
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
