f186a8ba58fb408f7458f1789beae835710feda0
Nandhitha-S19 <nandhitha.s1922@gmail.com>
2026-02-17 20:06:03 +0530

    pattern Medium

--- Round2/src/PatternFinding/Pattern1M.java
+++ Round2/src/PatternFinding/Pattern1M.java
@@ -1,5 +1,29 @@
 package PatternFinding;
 
 public class Pattern1M {
-    
+    public static void main(String[] args) {
+        //Given an integer n. You need to
+        // recreate the pattern given below for
+        // any value of N. Let's say for N = 5,
+        // the pattern should look like as below:
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //*********
+        int n = 5;
+        for(int i = 1; i <=n;i++){
+            //space
+            for(int j = 1;j <= n-i;j++){
+                System.out.print(' ');
+            }
+            for(int k = 1;k <= i;k++){
+                System.out.print('*');
+            }
+            for(int l = 1; l<i ; l++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
 }
