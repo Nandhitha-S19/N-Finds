01c0fb8f808c1c6fa5484c8ccd0c20a1ba37f054
Nandhitha-S19 <nandhitha.s1922@gmail.com>
2026-02-17 21:43:58 +0530

    pattern Medium

---
+++ Round2/src/PatternFinding/Pattern3M.java
@@ -0,0 +1,50 @@
+package PatternFinding;
+
+public class Pattern3M {
+    public static void main(String[] args) {
+        //Given an integer n. You need to recreate the pattern given
+        // below for any value of N.
+        // Let's say for N = 5, the pattern should look like as below:
+        //    *
+        //   ***
+        //  *****
+        // *******
+        //*********
+        //*********
+        // *******
+        //  *****
+        //   ***
+        //    *
+
+        int n = 5;
+        for(int i = 1; i <=n;i++){
+            for(int j = 1; j <= n-i;j++){
+                System.out.print(' ');
+            }
+            for(int j = 1; j <i;j++){
+                System.out.print('*');
+            }
+            System.out.print('*');
+
+            for(int j = 1; j <i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <i;j++){
+                System.out.print(' ');
+            }
+            for(int j = n; j >i;j--){
+                System.out.print('*');
+            }
+            System.out.print('*');
+            for(int j = n; j >i;j--){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
